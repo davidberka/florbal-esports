e1ddddab2d7688b0a2c8f201aed898a40fc30556
David Berka <davidberka2005@gmail.com>
2021-11-29 18:34:42 +0100

    Archive Page

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // MENU - START
 const burgerMenu = document.querySelector(".burger");
 const slideMenu = document.querySelector(".menu_container");
+const navLinks = document.querySelectorAll('.nav ul li');
 
 burgerMenu.addEventListener("click", () => {
   slideMenu.classList.toggle("active");
   burgerMenu.classList.toggle("active");
 });
+
+if (window.innerWidth < 1280) {
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      slideMenu.classList.remove("active");
+      burgerMenu.classList.remove("active");
+    })
+  })
+}
 // MENU - END
 
 // SCROLL TO - START
